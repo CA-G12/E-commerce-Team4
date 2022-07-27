d5afc42f9e35f655710d49ba8e4c239588c5bf4c
unknown <saritadh74@gmail.com>
2022-07-27 15:18:23 +0300

    added a failed test in test file relates #6

---
+++ test/buyer.test.js
@@ -0,0 +1,15 @@
+const { item } = require('../buyer/logic');
+
+test('test if the function return one element object', function () {
+  let products = [
+    {
+      id: '1a',
+      name: 'shirt',
+    },
+    {
+      id: '1b',
+      name: 'shirt',
+    },
+  ];
+  expect(item('1a', products)).toBe(2);
+});
